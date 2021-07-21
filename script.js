cc99f6a98cf0c8ab4dfe78e188307de4801712bb
opedroaravena <pedroaravena.contato@gmail.com>
2021-07-21 00:39:17 -0300

    Finishing with javascript

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ document.body.addEventListener('keyup', (event)=>{
     playSound(event.code.toLowerCase())
 })
 
+document.querySelector('.composer button').addEventListener('click', () =>{
+    let song = document.querySelector('#input').value;
+
+    if(song !== '') {
+        //separando strings e criando um array com o método split
+        let songArray = song.split('')
+        playComposition(songArray)
+    }
+})
+
 function playSound(sound){
     let audioElement = document.querySelector(`#s_${sound}`)
     let keyElement = document.querySelector(`div[data-key="${sound}"]`)
 
     if(audioElement) {
+        //som tocando e recarregando simultaneamente
+        audioElement.currentTime = 0;
         audioElement.play();
     }
 
@@ -19,5 +31,16 @@ function playSound(sound){
         setTimeout(()=>{
             keyElement.classList.remove('active')
         }, 300)
+    }
+}
+
+function playComposition(songArray){
+    let wait = 0
+    for(let songItem of songArray){
+        setTimeout(()=>{
+            playSound(`key${songItem}`)
+        }, wait)
+        
+        wait += 250
     }
 }
